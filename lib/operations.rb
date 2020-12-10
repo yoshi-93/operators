5fa46143a1d6f86dd31a5f4f7c6c2bbcf46316bc
Mario Lopez <ez0212sp@icloud.com>
2020-12-10 17:38:30 -0500

    commit tree 784cd6003fce076dd4aa427611bf975968eb6ff0

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,24 @@
-def unsafe?(speed)
 
+
+def unsafe?(speed = 60)
+    if speed > 60
+        return true
+    elsif speed < 40
+        return true
+    else speed == 40 && 60 
+        return false
+    end
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe?(speed = 60)
+    if speed > 60
+        return true
+    elsif speed < 40
+        return true
+    else speed == 40 && 60 
+        return false
+    end
 end
-	
-
+speed = 60  
+speed > 60 ? true : false
 
